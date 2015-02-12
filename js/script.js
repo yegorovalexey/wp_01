5fa36bdece0fc48dd1178750f69a7e3c6e2d4f78
yegorovalexey <egorov@citrus.com.ua>
2015-02-12 23:52:59 +0200

    Added instagram

--- js/script.js
+++ js/script.js
@@ -11,3 +11,28 @@ function scrollTopage_start(){
 }
 
 
+$(document).ready(function() {
+    $(".fancybox").fancybox();
+
+    /*var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'andreypredelin',
+        clientId: ' 	4a355b525f874ae3ad2088254e83ed0c'
+     userId:1485681255
+    });*/
+    var feed = new Instafeed({
+        get: 'user',
+        userId: 256184017,
+        clientId: ' 	4a355b525f874ae3ad2088254e83ed0c',
+        accessToken: '1485681255.467ede5.bc5b3e9121034b358e422a1179eb52b0',
+        template: '<div class="np item"><a class="fancybox_insta" rel="group_insta" href="{{link}}"><img  src="{{image}}"></a></div>',
+        after: function() {
+            $(".fancybox_insta").fancybox();
+        }
+    });
+    feed.run();
+
+});
+
+
+
